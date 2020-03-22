da4f7ed05c2d2d034ed561cced565e49296a603f
akoski <akoski@kth.se>
2020-03-22 21:25:11 +0100

    Add plot an index sentiment correlation

--- sentiment/report.py
+++ sentiment/report.py
@@ -6,13 +6,20 @@ from afinn import Afinn # Sentiment analysis
 import requests # Web requests
 import statistics
 import csv
+import matplotlib.pyplot as plt
+import string
+
+NUMBER_OF_REPORTS_TO_FETCH = 300
+SENTIMENTS = []
 
 class Report:
-    def __init__(self, text, scores, sentiment, date):
+    def __init__(self, text, scores, sentiment, date, openAt, closeAt):
         self.text = text
         self.sscores = scores
         self.sscore = float(sentiment)
         self.date = date
+        self.omxOpen = float(openAt.replace(",", "."))
+        self.omxClose = float(closeAt.replace(",", "."))
     def __str__(self):
         return self.date + " \t " + str(self.sscore) + " \t " + str(len(self.sscores))
 
@@ -21,25 +28,36 @@ class Report:
 def init_omxs30():
     # Headers; date;high;low;close;mean;vol;Oms;
     result = {}
-    with open('data/omxs30.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter = ";")
+    with open('data/newomxs30.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             result[row["date"]] = row
 
     # csv_reader.sort(key=lambda x: x["Datum"], reverse=True)
     return result
 
+def init_snp500():
+    # Headers; date;high;low;close;mean;vol;Oms;
+    result = {}
+    with open('data/snp500.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            dList = row["date"].split("/")
+            row["date"] = dList[2] + "-" + dList[0] + "-" + dList[1]
+            result[row["date"]] = row
 
-NUMBER_OF_REPORTS_TO_FETCH = 100
-SENTIMENTS = []
+
+    # csv_reader.sort(key=lambda x: x["Datum"], reverse=True)
+    return result
+
 
 def main():
     afinn = Afinn(language='sv', emoticons=True)
 
-    omx = init_omxs30()
+    #omx = init_omxs30()
+    omx = init_snp500()
+
     print(omx["2020-03-19"])
-    
-    return
 
     url = 'https://research.sebgroup.com/mapi/reports?reporttype=Morning%20Alert&nbrows='+str(NUMBER_OF_REPORTS_TO_FETCH)
     res = requests.get(url)
@@ -52,14 +70,34 @@ def main():
             report = report + j.text
         pub_date = res[i]["publishedDate"].split("T")[0]
         scores = afinn.scores_with_pattern(report)
-        SENTIMENTS.append(Report(report, scores, sum(scores), pub_date))
+        try:
+            SENTIMENTS.append(Report(report, scores, sum(scores), pub_date, omx[pub_date]["open"], omx[pub_date]["close"]))
+        except KeyError:
+            pass
 
     print(statistics.mean(map(lambda i: i.sscore, SENTIMENTS)))
     print("Date\tScore\tnrofwords\n")
+
+    x = []
+    y_avkastning = []
+    y_change = []
+
     for i in SENTIMENTS:
         print(i)
+        x.append(i.sscore)
+        y_change.append(i.omxClose - i.omxOpen)
 
     #tts = gTTS(SENTIMENTS[0].text, lang='sv')
     #tts.save('report.mp3')
+
+    fig, ax = plt.subplots()
+    ax.scatter(x, y_change, color="red")
+    ax.set_ylabel('snp500 Delta')
+    ax.set_xlabel('Sentiment')
+    ax.axhline(1, color="#bfbfbf")
+    ax.axvline(25, color="#bfbfbf")
+
+    fig.suptitle('Correlation snp500 delta / Sentiment of Morning Alert')
+    plt.show()
 
 main()
