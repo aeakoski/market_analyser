61acd3ea1a95fdb48158970925e628a06fd3554b
akoski <akoski@kth.se>
2020-03-29 22:28:21 +0200

    ML-PREPP: Add 2gram to report object

--- sentiment/report.py
+++ sentiment/report.py
@@ -9,8 +9,8 @@ import csv
 import matplotlib.pyplot as plt
 import string
 
-NUMBER_OF_REPORTS_TO_FETCH = 300
-SENTIMENTS = []
+NUMBER_OF_REPORTS_TO_FETCH = 6 #300
+REPORTS = []
 
 class Report:
     def __init__(self, text, scores, sentiment, date, openAt, closeAt):
@@ -20,10 +20,38 @@ class Report:
         self.date = date
         self.omxOpen = float(openAt.replace(",", "."))
         self.omxClose = float(closeAt.replace(",", "."))
+        self._2gram = {}
+        self._3gram = {}
     def __str__(self):
         return self.date + " \t " + str(self.sscore) + " \t " + str(len(self.sscores))
+    def create2gram(self):
+        cleanText = ""
+        for char in self.text:
+            if char.isalpha():
+                cleanText = cleanText + char.lower()
+            elif len(cleanText) and cleanText[-1] != " ":
+                cleanText = cleanText + " "
 
+        if cleanText[-1] == " ":
+            cleanText = cleanText[:-1]
 
+        print(cleanText)
+        cleanTextList = cleanText.split(" ")
+        for i in range(len(cleanTextList)):
+            if i+1 == len(cleanTextList):
+                break
+            try:
+                self._2gram[cleanTextList[i] + " " + cleanTextList[i+1]] += 1
+            except KeyError:
+                self._2gram[cleanTextList[i] + " " + cleanTextList[i+1]] = 1
+
+        toPrint = []
+        for i in self._2gram.keys():
+            toPrint.append((i, self._2gram[i]))
+        toPrint.sort(key = lambda x : x[1])
+
+        for i in toPrint:
+            print i[0]
 
 def init_omxs30():
     # Headers; date;high;low;close;mean;vol;Oms;
@@ -46,18 +74,15 @@ def init_snp500():
             row["date"] = dList[2] + "-" + dList[0] + "-" + dList[1]
             result[row["date"]] = row
 
-
     # csv_reader.sort(key=lambda x: x["Datum"], reverse=True)
     return result
 
-
 def main():
+    print("Start")
     afinn = Afinn(language='sv', emoticons=True)
 
-    #omx = init_omxs30()
-    omx = init_snp500()
-
-    print(omx["2020-03-19"])
+    omx = init_omxs30()
+    #omx = init_snp500()
 
     url = 'https://research.sebgroup.com/mapi/reports?reporttype=Morning%20Alert&nbrows='+str(NUMBER_OF_REPORTS_TO_FETCH)
     res = requests.get(url)
@@ -71,33 +96,48 @@ def main():
         pub_date = res[i]["publishedDate"].split("T")[0]
         scores = afinn.scores_with_pattern(report)
         try:
-            SENTIMENTS.append(Report(report, scores, sum(scores), pub_date, omx[pub_date]["open"], omx[pub_date]["close"]))
+            REPORTS.append(Report(report, scores, sum(scores), pub_date, omx[pub_date]["open"], omx[pub_date]["close"]))
         except KeyError:
+            # Possible error reasons
+            # OMX file is not up to date
+            # Date parseing is wrong
+
+            # print("ERROR: Key error")
+            # print(pub_date)
             pass
 
-    print(statistics.mean(map(lambda i: i.sscore, SENTIMENTS)))
-    print("Date\tScore\tnrofwords\n")
+    print("Done parsing")
+    for report in REPORTS:
+        print("Report")
+        report.create2gram()
 
-    x = []
-    y_avkastning = []
-    y_change = []
+    # print(statistics.mean(map(lambda i: i.sscore, REPORTS)))
+    # print("Date\tScore\tnrofwords\n")
 
-    for i in SENTIMENTS:
-        print(i)
-        x.append(i.sscore)
-        y_change.append(i.omxClose - i.omxOpen)
-
-    #tts = gTTS(SENTIMENTS[0].text, lang='sv')
+    # Get the report in MP3 format
+    #tts = gTTS(REPORTS[0].text, lang='sv')
     #tts.save('report.mp3')
 
-    fig, ax = plt.subplots()
-    ax.scatter(x, y_change, color="red")
-    ax.set_ylabel('snp500 Delta')
-    ax.set_xlabel('Sentiment')
-    ax.axhline(1, color="#bfbfbf")
-    ax.axvline(25, color="#bfbfbf")
 
-    fig.suptitle('Correlation snp500 delta / Sentiment of Morning Alert')
-    plt.show()
+    # Plot sentiment market correlation
+
+    # x = []
+    # y_avkastning = []
+    # y_change = []
+    #
+    # for i in REPORTS:
+    #     print(i)
+    #     x.append(i.sscore)
+    #     y_change.append(i.omxClose - i.omxOpen)
+
+    # fig, ax = plt.subplots()
+    # ax.scatter(x, y_change, color="red")
+    # ax.set_ylabel('snp500 Delta')
+    # ax.set_xlabel('Sentiment')
+    # ax.axhline(1, color="#bfbfbf")
+    # ax.axvline(25, color="#bfbfbf")
+    #
+    # fig.suptitle('Correlation snp500 delta / Sentiment of Morning Alert')
+    # plt.show()
 
 main()
